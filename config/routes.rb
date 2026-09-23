486c821c06486f8cd80e707ba75b365ac5d76fde
oscaregr <oegr.el.may@gmail.com>
2026-09-22 22:01:15 -0600

    primera refactorizacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  delete "/products/:id", to: "products#destroy"
-  patch "/products/:id", to: "products#update"
-  post "/products", to: "products#create"
-  get "/products/:id/edit", to: "products#edit", as: :edit_product
-  get "/products/new", to: "products#new", as: :new_product
-  get "/products", to: "products#index"
-  get "/products/:id", to: "products#show", as: :product
+  # delete "/products/:id", to: "products#destroy"
+  # patch "/products/:id", to: "products#update"
+  # post "/products", to: "products#create"
+  # get "/products/:id/edit", to: "products#edit", as: :edit_product
+  # get "/products/new", to: "products#new", as: :new_product
+  # get "/products", to: "products#index"
+  # get "/products/:id", to: "products#show", as: :product
 
+  resources :products, path: "/"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
